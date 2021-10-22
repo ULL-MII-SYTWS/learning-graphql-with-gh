3b0249f35746f27b077cc89701634e4c1e2dfd35
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2021-10-22 08:15:33 +0100

    org-repo-list searchtoptenstar

--- searchtoptenstar/searchtoptenstar.bash
+++ searchtoptenstar/searchtoptenstar.bash
@@ -1,3 +1,4 @@
+# https://katopz.medium.com/how-to-search-with-github-graphql-e6c142dc61ed
 gh api graphql --paginate \
    --field queryString="language:JavaScript stars:>10000" \
    --field query=@searchtoptenstar.gql
