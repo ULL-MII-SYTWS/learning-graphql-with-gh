95c548d94d2063fd47988f55f8931b0d480949bd
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2021-10-22 08:29:26 +0100

    org-repo-list searchtoptenstar

--- searchtoptenstar/searchtoptenstar.bash
+++ searchtoptenstar/searchtoptenstar.bash
@@ -1,4 +1,5 @@
 # https://katopz.medium.com/how-to-search-with-github-graphql-e6c142dc61ed
+# Does not work when using GRaphiQL (timeout). May be needs a token with privileges?
 gh api graphql --paginate \
    --field queryString="language:JavaScript stars:>10000" \
    --field query=@searchtoptenstar.gql
