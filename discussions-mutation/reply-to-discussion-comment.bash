dd27143c62329e0525233313a8ad6e4c65003be6
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-11-28 09:33:08 +0000

    get-comments.bash

---
+++ discussions-mutation/reply-to-discussion-comment.bash
@@ -0,0 +1,28 @@
+#!/bin/bash 
+BODY="Contador de cuerpo: 3"
+discussionId="D_kwDOGLyMF84ARkhY"
+replyToId="DC_kwDOGLyMF84AQN01"
+if [[ $# != 0 ]]; then
+  BODY=$1
+fi
+gh api graphql \
+-H 'GraphQL-Features: discussions_api' \
+-f body="Contador de cuerpo: $BODY" \
+-F discussionId=$discussionId \
+-F replyToId=$replyToId \
+-f query='
+mutation addComment($body: String!, $discussionId: ID!, $replyToId: ID!){
+  addDiscussionComment(input:
+    {
+      body: $body , 
+      discussionId: $discussionId, 
+      replyToId: $replyToId 
+    }
+  )
+  {
+    comment{
+      body
+      id
+    }
+  }
+}'
