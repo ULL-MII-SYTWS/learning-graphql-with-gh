d0feb2c743e78dfb4c8561eccd2506fc50634dea
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-11-29 12:50:35 +0000

    pagination

--- gh-api-example/gh-api-example.bash
+++ gh-api-example/gh-api-example.bash
@@ -1,19 +1,35 @@
 #!/bin/bash
+ORG="ULL-MII-SYTWS-2021" 
+LAB="p01-t1-iaas-alu0101040882"
 
-help() {
-cat << HELP
-To know about API Pagination and GraphQL Connections follow these links:
+POSITIONAL_ARGS=()
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -o|--org)
+     ORG=$2
+     shift # past argument
+     shift # past value
+     ;;
+    -l|--lab)
+      LAB=$2
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "USAGE"
+      echo "  $0 [options]"
+      echo "Available options:"
+      echo "  -o --ORG <GitHub Organization>"
+      echo "  -l --lab <repository>"
+      echo "Example:"
+        echo "  $0 -o \"$ORG\" -l \"$LAB\""
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
 
-    * <https://www.apollographql.com/blog/graphql/explaining-graphql-connections/>
-    * <https://ignaciochiazzo.medium.com/paginating-requests-in-apis-d4883d4c1c4c#:~:text=When%20exposing%20large%20data%20sets,endpoints%2C%20index%20endpoints%2C%20etc.>
-
-HELP
-}
-
-case $1 in
--h | -H | --help ) help
-    exit
-    ;;
-esac
-
-gh api graphql --paginate -F query=@gh-api-example.gql $@
+gh api graphql --paginate -F owner="$ORG" -F lab="$LAB" -F query=@gh-api-example.gql $POSITIONAL_ARGS
