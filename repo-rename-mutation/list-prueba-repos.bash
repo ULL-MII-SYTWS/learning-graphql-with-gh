3f1adc0e6967fb48274ef8c575cf690b6be531cc
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-12-14 15:23:39 +0000

    example to rename a repo in graphql

--- repo-rename-mutation/list-prueba-repos.bash
+++ repo-rename-mutation/list-prueba-repos.bash
@@ -1,2 +1,3 @@
-# install gh extension org-browse-repo
-gh org-browse-repo -S prueba -d -o ULL-ESIT-DMSI-1920
+# Description: List all repos in the organization prueba
+# requirement: install gh extension org-browse-repo
+gh org-browse-repo -S prueba -d -o ULL-ESIT-DMSI-1920 |  sed -nE 's/gh browse -R|$//p' | sort
